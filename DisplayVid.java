a0c5cd1f7bd4d418c7e0263b98f6c7e29c17428a
Adamwgoh <adamwgoh@gmail.com>
2014-01-31 00:33:16 +0800

    Image difference video feed, fixed drop frame rate due to lack of one image difference every three frames. Will continue to work on motion history image

--- DisplayVid.java
+++ DisplayVid.java
@@ -11,11 +11,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
+import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
 import org.opencv.highgui.VideoCapture;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.video.Video;
 
 
 
@@ -27,14 +30,15 @@ public class DisplayVid {
 		
 		Mat frame1 = new Mat();
 		Mat frame2 = new Mat();
-		Mat frame3 = new Mat();
+		Mat mhi = new Mat();
 		Mat framediff = new Mat();
 		VideoCapture cap = new VideoCapture();
 		BufferedImage img = null;
 		MatToBufferedImage conv = new MatToBufferedImage();
+		int count = 0;
 		
-		//cap.open(0);
-		cap.open("goalvid.mp4");
+		cap.open(0);
+		//cap.open("goalvid.mp4");
 		
 		if(!cap.isOpened()){
 			System.err.println("Error opening file!");
@@ -42,9 +46,11 @@ public class DisplayVid {
 			System.out.println("Camera ok");
 		}
 		
-		cap.read(frame1);
-		cap.read(frame2);
+			cap.read(frame1);
+			cap.read(frame2);
 			Core.absdiff(frame1, frame2, framediff);
+			Imgproc.cvtColor(frame1, frame2, Imgproc.COLOR_RGB2GRAY);
+			Imgproc.cvtColor(frame1, frame1, Imgproc.COLOR_RGB2GRAY);
 			conv.SetImage(framediff);
 			img = conv.getImage();
 
@@ -54,19 +60,36 @@ public class DisplayVid {
 			ColorPanel panel = new ColorPanel(img);
 			jf.setContentPane(panel);
 			jf.setVisible(true);
+			double timestamp = (System.nanoTime()) / 1e9;
+			mhi = Mat.zeros(new Size(img.getWidth(), img.getHeight()), CvType.CV_32FC1);
 			
-			while(cap.isOpened()){
+			while(true){
+				if(count % 2 == 0){
 				cap.read(frame1);
-				cap.read(frame2);
-				Imgproc.cvtColor(frame1, frame1, Imgproc.COLOR_RGB2GRAY);
-				Imgproc.cvtColor(frame2, frame2, Imgproc.COLOR_RGB2GRAY);
-				Core.absdiff(frame2, frame1, framediff);
-				conv.SetImage(framediff);
-				img = conv.getImage();
-				panel.setImage(img);
-				panel.repaint();
+				
+					Imgproc.cvtColor(frame1, frame1, Imgproc.COLOR_RGB2GRAY);
+					Core.absdiff(frame1, frame2, framediff);
+					//Imgproc.threshold(framediff, framediff, Imgproc.THRESH_OTSU, 1, Imgproc.THRESH_BINARY);
+					
+					//Video.updateMotionHistory(framediff, mhi, timestamp, 1);
+					img = conv.getImage(framediff);
+					panel.setImage(img);
+					panel.repaint();
+					count++;
+				}else if(count % 2 != 0){
+					cap.read(frame2);
+					Imgproc.cvtColor(frame2, frame2, Imgproc.COLOR_RGB2GRAY);
+					Core.absdiff(frame1, frame2, framediff);
+					//Imgproc.threshold(framediff, framediff, Imgproc.THRESH_OTSU, 1, Imgproc.THRESH_BINARY);
+					
+					//Video.updateMotionHistory(framediff, mhi, timestamp, 1);
+					img = conv.getImage(framediff);
+					panel.setImage(img);
+					panel.repaint();
+					count++;
+				}
 			}
-
+			
 	
 	
 	}
